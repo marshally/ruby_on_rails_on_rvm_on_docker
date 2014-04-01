ab8d4bf789b34525b7e59d9690d3db46c54ed142
Marshall Yount <marshall@yountlabs.com>
2014-04-01 14:36:45 -0500

    rails new

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyOnRailsOnRvmOnDocker::Application.config.session_store :cookie_store, key: '_ruby_on_rails_on_rvm_on_docker_session'
